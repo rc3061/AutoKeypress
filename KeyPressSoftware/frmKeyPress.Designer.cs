cc19935c81b487298e0ad1ebffad08121f928415
Ryan Costello <ryan.costello@lighting.com>
2018-03-04 11:42:49 -0800

    initial checkin

---
+++ KeyPressSoftware/frmKeyPress.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class frmKeyPress
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmKeyPress));
+            this.mainTimer = new System.Windows.Forms.Timer(this.components);
+            this.IdleTimerTxt = new System.Windows.Forms.TextBox();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // mainTimer
+            // 
+            this.mainTimer.Tick += new System.EventHandler(this.mainTimer_Tick);
+            // 
+            // IdleTimerTxt
+            // 
+            this.IdleTimerTxt.Location = new System.Drawing.Point(114, 55);
+            this.IdleTimerTxt.Name = "IdleTimerTxt";
+            this.IdleTimerTxt.Size = new System.Drawing.Size(120, 20);
+            this.IdleTimerTxt.TabIndex = 0;
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "notifyIcon1";
+            this.notifyIcon.Visible = true;
+            this.notifyIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon_MouseDoubleClick);
+            // 
+            // frmKeyPress
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(464, 142);
+            this.Controls.Add(this.IdleTimerTxt);
+            this.Name = "frmKeyPress";
+            this.Text = "key press";
+            this.Load += new System.EventHandler(this.frmKeyPress_Load);
+            this.Resize += new System.EventHandler(this.frmKeyPress_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer mainTimer;
+        private System.Windows.Forms.TextBox IdleTimerTxt;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+    }
+}
+
---
+++ KeyPressSoftware/frmKeyPress.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace WindowsFormsApplication1
+{
+    public partial class frmKeyPress : Form
+    {
+        public frmKeyPress()
+        {
+            InitializeComponent();
+        }
+
+        private void mainTimer_Tick(object sender, EventArgs e)
+        {
+            this.IdleTimerTxt.Text = IdleTimeFinder.GetIdleTime().ToString();
+            if(IdleTimeFinder.GetIdleTime() > 12000) {
+                SendKeys.Send("{F15}");
+            }
+        }
+
+        private void frmKeyPress_Load(object sender, EventArgs e)
+        {
+            this.mainTimer.Start();
+        }
+
+        private void frmKeyPress_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                notifyIcon.Visible = true;
+              // notifyIcon.ShowBalloonTip(3000);
+                this.ShowInTaskbar = false;
+            }
+        }
+
+        private void notifyIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.WindowState = FormWindowState.Normal;
+            this.ShowInTaskbar = true;
+            notifyIcon.Visible = false;
+        }
+    }
+
+
+
+    internal struct LASTINPUTINFO
+    {
+        public uint cbSize;
+
+        public uint dwTime;
+    }
+
+    /// <summary>
+    /// Helps to find the idle time, (in ticks) spent since the last user input
+    /// </summary>
+    public class IdleTimeFinder
+    {
+        [DllImport("User32.dll")]
+        private static extern bool GetLastInputInfo(ref LASTINPUTINFO plii);
+
+        [DllImport("Kernel32.dll")]
+        private static extern uint GetLastError();
+
+        public static uint GetIdleTime()
+        {
+            LASTINPUTINFO lastInPut = new LASTINPUTINFO();
+            lastInPut.cbSize = (uint)System.Runtime.InteropServices.Marshal.SizeOf(lastInPut);
+            GetLastInputInfo(ref lastInPut);
+
+            return ((uint)Environment.TickCount - lastInPut.dwTime);
+        }
+        /// <summary>
+        /// Get the Last input time in ticks
+        /// </summary>
+        /// <returns></returns>
+        public static long GetLastInputTime()
+        {
+            LASTINPUTINFO lastInPut = new LASTINPUTINFO();
+            lastInPut.cbSize = (uint)System.Runtime.InteropServices.Marshal.SizeOf(lastInPut);
+            if (!GetLastInputInfo(ref lastInPut))
+            {
+                throw new Exception(GetLastError().ToString());
+            }
+            return lastInPut.dwTime;
+        }
+
+
+
+    }
+
+
+}
+
+
